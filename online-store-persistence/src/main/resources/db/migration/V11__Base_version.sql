dcebfd06bf3e41e2833e421e760b08de24ab6c3b
Olga Seledtsova <olgaseledtsova9@yandex.by>
2025-08-26 18:37:46 +0300

    some changes

--- online-store-persistence/src/main/resources/db/migration/V11__Base_version.sql
+++ online-store-persistence/src/main/resources/db/migration/V11__Base_version.sql
@@ -1,6 +1,7 @@
 insert into item (id, name, price, description)
-values (nextval('item_id_seq'), 'Inflatable chaise lounge chair', 96.00,'Constructed from durable, superior vinyl, the chair can support loads up to 125kg..' ),
-       (nextval('item_id_seq'), 'Inflatable raft', 115.00,'Wings - made in photorealistic design. Made specifically for those who like to be in the spotlight, these wings will not leave anyone indifferent..' ),
-       (nextval('item_id_seq'), 'Swimming goggles', 15.00,'Made from hypoallergenic plastic with an anti-fog coating.' ),
-       (nextval('item_id_seq'), 'Inflatable mattress', 39.00,'Classic model, simple and economical option for home or travel.' ),
-       (nextval('item_id_seq'), 'Swimming snorkel mask', 39.00,'Underwater life is hidden from our eyes, which is why it is so interesting to watch fish, corals and other underwater inhabitants.' );
+values
+    (nextval('item_id_seq'), 'Spinning rod', 85.00, 'Lightweight carbon spinning rod, designed for both freshwater and saltwater fishing.'),
+    (nextval('item_id_seq'), 'Fishing reel', 65.00, 'Reliable reel with smooth drag system and anti-reverse function for comfortable fishing.'),
+    (nextval('item_id_seq'), 'Set of fishing lures', 25.00, 'Assortment of colorful lures imitating small fish, effective for predator fishing.'),
+    (nextval('item_id_seq'), 'Fishing line 150m', 12.00, 'Durable monofilament fishing line, length 150 meters, high resistance to abrasion.'),
+    (nextval('item_id_seq'), 'Tackle box', 30.00, 'Compact plastic box with compartments for storing hooks, sinkers, and other fishing accessories.');
